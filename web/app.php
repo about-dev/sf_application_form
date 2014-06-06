fa5a3a3e2ca9e2f0a5f59b14347c69e870b3cfbb
Claudia Negreanu <claudia.negreanu@emag.ro>
2014-06-06 14:24:07 +0300

    using Symfony Form Component

--- web/app.php
+++ web/app.php
@@ -17,7 +17,7 @@ umask(0000);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('dev', false);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
